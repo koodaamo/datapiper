ce7c4a2600705956f43a6904ab83a4d22b598d47
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-10-30 21:34:01 +0200

    refactored the pipe generation

--- datapiper/piper.py
+++ datapiper/piper.py
@@ -11,41 +11,37 @@ logging.basicConfig()
 def piper(ops, source=None, sink=None):
    "task runner posing as a generator (pull) or a coroutine (push)"
 
-   # pull mode; operate as a generator
+   # pipeline global metadata
+   meta = {}
+
+   # construct the coroutine pipeline, last task first
+   end = ending(meta, sinkcallable=sink)
+   tasks = [end]
+
+   successor = end
+
+   for taskop in ops[::-1]:
+      tsk = task(taskop, successor)
+      tasks.insert(0, tsk)
+      successor = tsk
+
+   # pull mode; provide a generator
    if source and not sink:
 
-      # construct the coroutine pipeline, last task first
-      meta = {}
-      end = ending(meta)
-      tasks = [end]
-      successor = end
-      for taskop in ops[::-1]:
-         tsk = task(taskop, successor)
-         tasks.insert(0, tsk)
-         successor = tsk
-
-      # run the generator
       def generate(first):
          for data in source:
             # push each record into the pipeline
             first.send((meta, data))
+            # and yield the result
             yield meta["result"]["data"]
 
       return generate(tasks[0])
 
-   # push mode; operate as a coroutine
+   # push mode; provide a coroutine
    elif sink and not source:
-      meta = {}
-      end = ending(meta, sinkcallable=sink)
-      successor = end
-      tasks = [end]
-      for taskop in ops[::-1]:
-         tsk = task(taskop, successor)
-         tasks.insert(0, tsk)
-         successor = tsk
       begin = beginning(tasks[0], meta)
       tasks.insert(0, begin)
-      return begin
+      return begin # ie. the first task of the pipeline
 
    else:
       raise PipelineException("invalid opertion mode")
